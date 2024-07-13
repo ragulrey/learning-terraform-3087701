8a0421a54d03e1848732e941c0d644e4a5860175
ragulrey <59682263+ragulrey@users.noreply.github.com>
2024-07-13 22:17:18 +0100

    Alb and Auto scaling

--- main.tf
+++ main.tf
@@ -14,19 +14,14 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]  
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
 
   tags = {
     Terraform = "true"
@@ -34,57 +29,55 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  
+  #vpc subnet
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
+  name = "blog-alb"
 
-  # Security Group
-  security_groups = [module.blog_sg.security_group_id]
+  load_balancer_type = "application"
 
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = [
-        {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      ]
     }
   ]
 
-  listeners = [
+  http_tcp_listeners = [
     {
-      port = 80
-      protocol = "HTTP"
-      default_action = {
-        type             = "forward"
-        target_group_arn = module.blog_alb.target_group_arns[0]
-      }
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
   ]
 
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
 }
 
